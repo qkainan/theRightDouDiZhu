19c5bd4915ec5a1aa30ee2da6bbc44e69191c8ba
Kannen <qkn0208@webmail.hzau.edu.cn>
2023-02-11 21:31:37 +0800

    Initial commit

---
+++ src/com/qkainan/domain/Poker.java
@@ -0,0 +1,17 @@
+package com.qkainan.domain;
+
+public class Poker {
+//    * PockerCard 类
+//* 功能：扑克牌的核心类 包括创建角色、创建牌、显示牌、洗牌、比较牌、排序牌等
+//* 属性：List cards 整幅扑克牌的集合 、 People[] peoples 角色
+//* 方法： public PockerCard() //无参构造函数： 创建牌的集合、创建两个角色对象
+//* public void initializeCard() //创建整幅扑克牌
+//* public void showCard() //显示单支牌的花色和点数
+//* public void refreshCard() //洗牌 从第一支牌开始从所有牌中随机取一只与之交换
+//* public void addPeopleInfo() //添加玩家的基本信息
+//* public void dealCard() //发牌 从开始分别给A B发牌 每人两支
+//* public void showPeopleCard() //显示两个角色自己获得的牌
+//* public void sortCard() //将每个角色自己的牌按从小到大排序
+//* public void comparePeopleCard() //比较角色的牌
+*
+}
---
+++ src/com/qkainan/domain/User.java
@@ -0,0 +1,8 @@
+package com.qkainan.domain;
+
+public class User {
+//    * People类
+//* 功能：产生一个角色
+//* 属性：ID、Name、和、存储牌的List集合。
+//            * 方法： public People() //初始化存储牌的List集合；
+}
