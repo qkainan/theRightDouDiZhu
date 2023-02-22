77779db012ae4e11e38ecae1998d55585d83db64
Kannen <qkn0208@webmail.hzau.edu.cn>
2023-02-22 13:41:58 +0800

    Initial commit

--- src/com/qkainan/domain/Commen.java
+++ src/com/qkainan/domain/Commen.java
@@ -74,8 +74,70 @@ public class Common {
                 return CardType.ct0;
             case 8:
                 if (isStraight(cards) && isPairStraight(cards)) {
-                    return CardType.ct0；
+                    return CardType.ct1122;
+                } else if (isPlane(cards)) {
+                    return CardType.ct111222;
+                } else if (isPlaneWithSingle(cards)) {
+                    return CardType.ct11122234;
+                } else if (isPlaneWithPair(cards)) {
+                    return CardType.ct1112223344;
                 }
         }
+        return CardType.ct0;
     }
+
+    /**
+     * 判断给定的牌是否为顺子。
+     *
+     * @param cards 牌列表
+     * @return 如果牌列表为顺子，返回true；否则返回false。
+     */
+    private boolean isStraight(List<Integer> cards) {
+        // 牌的数量必须为5或更多
+        if (cards.size() < 5) {
+            return false;
+        }
+
+        // 首先将牌按牌面值从小到大排序
+        Collections.sort(cards, Comparator.comparingInt(card -> card % 100));
+
+        // 判断牌是否连续
+        for (int i = 0; i < cards.size() - 1; i++) {
+            int currentCard = cards.get(i) % 100;
+            int nextCard = cards.get(i + 1) % 100;
+            if (nextCard - currentCard != 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * 判断给定的牌是否为连对。
+     *
+     * @param cards 牌列表
+     * @return 如果牌列表为连对，返回true；否则返回false。
+     */
+    private boolean isPairStraight(List<Integer> cards) {
+        // 牌的数量必须为偶数
+        if (cards.size() % 2 != 0) {
+            return false;
+        }
+
+        // 将牌按牌面值从小到大排序
+        Collections.sort(cards, Comparator.comparingInt(card -> card % 100));
+
+        // 判断牌是否为连续的对子
+        for (int i = 0; i < cards.size() - 1; i += 2) {
+            int currentCard = cards.get(i) % 100;
+            int nextCard = cards.get(i + 1) % 100;
+            if (nextCard - currentCard != 0 || (i > 0 && currentCard - cards.get(i - 1) % 100 != 0)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
 }
